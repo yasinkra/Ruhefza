c8928ddf56766e2f189078784172a73f07bf434f
yasinkra <yasinkara3665@gmail.com>
2026-03-03 13:12:35 +0300

    fix(pwa): rename pwa icons to bust cache and ensure main logo is used

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#0d9488',
         icons: [
             {
-                src: '/icon-192x192.png',
+                src: '/pwa-icon-192.png',
                 sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: '/icon-512x512.png',
+                src: '/pwa-icon-512.png',
                 sizes: '512x512',
                 type: 'image/png',
             },
